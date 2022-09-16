916bab9873a1386c9c8470ec4f57af75f7a0c0ed
unknown <dvlaxamana.student@ua.edu.ph>
2022-09-16 22:31:02 +0800

    fifth commit

--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,82 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am 20 years old and I am from Poblacion Sto. Tomas Pampanga. My hobbies include
-        playing video games and assembling/painting Gundam Plastic Model kits, also making papercraft models.
-        I am also learning how to cook.</p>
+      <h3>Contact Information: </h3>
+      <p>#08 J. Cordero Street Poblacion Sto. Tomas Pampanga </p>
+      <p>09171676577</p>
+      <p>deejaylaxamana@gmail.com</p>
+
+      <h3>Description: </h3>
+        <p>Computer Engineering Professional with over 10 years of experience specializing in CpE derpartment management for international logistics companies. I can implement effective IT strategies at local and global levels. My greatest strength is business awareness, which enables me to permanently streamline infrastructure and applications. Seeking to 
+        leverage my CpE management abilities in SanCorp.</p>
+        <h3>Skills: </h3>
 
 
   
 
         <p>
-          I took up Computer Engineering beucase it is something I always wanted to become ever since I was a little
-          even though I had a vague idea of what a CpE does. I see myself in ten years as someone who has gained numerous skills
-          and experiences that have a decently high position in a company, or even a government agency.
-
+        <li>Project Scheduling</li>
+        <li>Strategic Planning</li>
+        <li>Sales Analysis</li>
+        <li>Team Management</li>
+        <li>Code Review</li>
         </p>
-        <p>
-
-          As a continuation of our SoftDev subject, I expect to learn a continuation of some of our previous knowledge.
-          But for me personally, I am fascinated by websites with very eye catching, aesthetically pleasing user interface
-          and want to be able to learn how to do the same as it did not get tackled very much on our previous semester.
+        <h3>Education:</h3>
+  
+   <div>
+        <p style={{textAlign: "left"}}><b>Tertiary</b></p>
+        <p style={{textAlign: "left"}}>
+        Bachelor of Science in Computer Engineering (Degree)
         </p>
+        <p style={{textAlign: "left"}}>
+        University of the Assumption
+        </p>
+        <p style={{textAlign: "left"}}>
+        Unisite Subdivision, Barangay Del Pilar, City of San Fernando Pampanga
+        </p>
+        <p style={{textAlign: "left"}}>
+        2019-present
+        </p>
+      </div>
+
+      <div>
+        <p style={{textAlign: "left"}}><b>Secondary</b></p>
+        <p style={{textAlign: "left"}}>
+        San Matias National High School
+        </p>
+        <p style={{textAlign: "left"}}>
+       L. Gomez Subd, San Matias Sto. Tomas, Pampanga SANTO TOMAS, PAMPANGA
+        </p>
+        <p style={{textAlign: "left"}}>
+        2017-2019 (Senior High School)
+        </p>
+        <br></br>
+        <p style={{textAlign: "left"}}>
+        San Matias National High School
+        </p>
+        <p style={{textAlign: "left"}}>
+        L. Gomez Subd, San Matias Sto. Tomas, Pampanga SANTO TOMAS, PAMPANGA
+        </p>
+        <p style={{textAlign: "left"}}>
+        2013-2017 (Junior High School)
+        </p>
+      </div>
+
+        <div>
+        <p style={{textAlign: "left"}}><b>Primary</b></p>
+        <p style={{textAlign: "left"}}>
+        Sto. Tomas Elementary School
+        </p>
+        <p style={{textAlign: "left"}}>
+        XPQ6+J35,Santo Tomas, Pampanga
+        </p>
+        <p style={{textAlign: "left"}}>
+        2006-2013
+        </p>
+      </div>
+
+
+
       </section>
     </Layout>
   );
